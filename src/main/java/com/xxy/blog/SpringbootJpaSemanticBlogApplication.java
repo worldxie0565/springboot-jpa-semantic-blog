6e105e685fe50c99f18cffa7c867d3a395ac486e
Administrator <admin@example.com>
2020-11-25 10:00:09 +0800

    1st commit

---
+++ src/main/java/com/xxy/blog/SpringbootJpaSemanticBlogApplication.java
@@ -0,0 +1,13 @@
+package com.xxy.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootJpaSemanticBlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootJpaSemanticBlogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/xxy/blog/SpringbootJpaSemanticBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xxy.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootJpaSemanticBlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
